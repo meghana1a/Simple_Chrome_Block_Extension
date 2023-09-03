1e3cdf7edefcd33251c51266bb1aefcd8e1eb535
meghana1a <113300508+meghana1a@users.noreply.github.com>
2023-09-03 15:22:58 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+switch (window.location.hostname) {
+    case "www.youtube.com":
+        document.body.innerHTML = "404";
+        break;
+    case "discord.com":
+        document.body.innerHTML = "404";
+        break;
+}
